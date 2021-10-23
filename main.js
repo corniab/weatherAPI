9ca8292f5f2ca29a99f40638e7bcd040b01fa80e
benc <ben.cornia@gmail.com>
2021-10-23 10:46:35 -0600

    added title

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 // Get the latitude and longitude of IP address
-// Fetch the National Weather Service /points/ which returns the correct forecast api for your location
+// Fetch the National Weather Service /points/ 
+// which returns the correct forecast api for your location
 const locationApi = 'https://ipapi.co/json/'
 fetch(locationApi).then(response => response.json())
                   .then(coords => {
+                      let location = document.getElementById("location-title")
+                      location.textContent = `${coords.city} Local Weather`
                       const weatherApi = `https://api.weather.gov/points/${coords["latitude"]},${coords["longitude"]}`
                       return fetch(weatherApi)
                     })
@@ -12,28 +15,30 @@ fetch(locationApi).then(response => response.json())
                        return fetch(forecastApi)
                    })
                    .then(response => response.json())
-                   .then(forecast => output(forecast["properties"]["periods"]));
+                   .then(forecast => output(forecast));
 
 // Output data to document
 function output(forecast) {
-  for (let i = 0; i < forecast.length; i++) {
+
+  let periods = forecast["properties"]["periods"]
+  for (let i = 0; i < periods.length; i++) {
     
     let article = document.createElement("article")
     article.className = "card"  
 
     let header = document.createElement("header")
     let heading = document.createElement("h2")
-    heading.textContent = forecast[i].name
+    heading.textContent = periods[i].name
 
     let img = document.createElement("img")
-    img.src = forecast[i].icon
+    img.src = periods[i].icon
     img.alt = "Weather Forecast"
 
     let div = document.createElement("div")
     div.className = "content"
 
     let description = document.createElement("p")
-    description.textContent = forecast[i].detailedForecast
+    description.textContent = periods[i].detailedForecast
 
     header.append(heading)
     div.append(description)
